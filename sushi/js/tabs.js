f09ecddb0db432c1d485308f18b61140819b3fc2
Ирина Попова <janneteldenorr@gmail.com>
2020-11-22 15:44:03 +0700

    first commit. add files

---
+++ sushi/js/tabs.js
@@ -0,0 +1,33 @@
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict'
+
+    let links = document.querySelectorAll('#links'),
+        cards = document.querySelectorAll('.card'),
+        menu = document.querySelector('.menu');
+
+    function hideContent(a) {
+        for(let i = a; i < cards.length; i++){
+            cards[i].classList.remove('show');
+            cards[i].classList.add('hide');
+        }
+    }
+
+    function showContent(a) {
+        if (cards[a].classList.contains('hide')) {
+            cards[a].classList.remove('hide');
+            cards[a].classList.add('show');
+        }
+    }
+
+    menu.addEventListener('click', function(e) {
+        let target = e.target;
+        for (let i = 0; i < links.length; i++) {
+            if (target == links[i]) {
+                hideContent(0);
+                showContent(i);
+            }
+        }
+    });
+
+    hideContent(1);
+});
